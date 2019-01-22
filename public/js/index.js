4ace35a867c54efc7a12fe6c90bba3e234f9e254
Raul Alcantara <ralcanta@mit.edu>
2019-01-22 12:33:13 -0500

    so close

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT_START = 'https://flappytim.herokuapp.com';
+//const API_ENDPOINT_START = 'https://flappytim.herokuapp.com';
 function main() {
     get('/api/whoami', {}, function(user) {
       renderNavbar(user);
